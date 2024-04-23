d29c4a024c15da28b513fc09992db605841597b7
yanon <yanon.dev@gmail.com>
2024-04-23 14:27:37 +0300

    shortening + json manager

--- link_shortener.py
+++ link_shortener.py
@@ -1,18 +1,25 @@
+import uuid 
 from json_manager import JsonManager
 
-
 class LinkShortener:
-
     @classmethod
     def get_link_by_id(self,  id: str):
-        link_entities = JsonManager.read()
-        for link_entity in link_entities:
-            if link_entity["id"] == id:
-                return link_entity["url"]
+        url_entities = JsonManager.read()
+        for url_entity in url_entities:
+            if url_entity["id"] == id:
+                return url_entity["url"]
         
         raise Exception("Your ID is not found")
 
-    # def short(self, )
+    @classmethod
+    def shorten(self, url: str):
+        unique_id = str(uuid.uuid4())[:8]
+        new_url_entity = {"id": unique_id, "url": url}
+        url_entities = JsonManager.read()
+        url_entities.append(new_url_entity)
+        JsonManager.write(url_entities)
+        return unique_id
+
 
 
 
--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import json
-
-name_to_search = input("Enter a name and I will give you his info: ")
-found_contact = None 
-
-with open("my_info.json", "r") as file:
-    data = json.load(file)
-
-for contact in data["contacts"]:
-    if contact["name"] == name_to_search:
-        print(f'Your information is {contact}')
-        found_contact = contact
-        break
-
-if not found_contact:
-    age = int(input("Enter new contact age: "))
-    data["contacts"].append({
-        "name": name_to_search, 
-        "age": age
-    })
+from link_shortener import LinkShortener
 
 
-print("stop here")
-with open("my_info.json", "w") as file:
-    json.dump(data, file)
+urls = [
+    "https://walla.co.il",
+    "https://nana.co.il"
+]
+
+for url in urls:
+    id = LinkShortener.shorten(url)
+    print(id)
