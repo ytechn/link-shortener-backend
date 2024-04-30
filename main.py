bcf87b7c1a5ba1b6e128cd728d7c963721f21ec2
cyber <yinnonh@technion.ac.il>
2024-04-30 23:10:29 +0300

    healthcheck + 301

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from link_shortener import LinkShortener
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+@app.get("/health_check")
+def health_check():
+    return "healthy"
+
+@app.get("/{id}")
+def health_check(id: str):
+    url = LinkShortener.get_link_by_id(id)
+    return RedirectResponse(url)
 
 
-urls = [
-    "https://walla.co.il",
-    "https://nana.co.il",
-    "https://ynet.co.il"
-]
 
-for url in urls:
-    id = LinkShortener.shorten(url)
-    print(id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# urls = [
+#     "https://walla.co.il",
+#     "https://nana.co.il",
+#     "https://ynet.co.il"
+# ]
+
+# for url in urls:
+#     id = LinkShortener.shorten(url)
+#     print(id)
