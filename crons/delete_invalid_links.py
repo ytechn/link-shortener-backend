aef675958e4c920d16001f9c33e2ce37ee02e135
cyber <yinnonh@technion.ac.il>
2024-05-10 14:39:54 +0300

    added start of delete invalid links

--- crons/delete_invalid_links.py
+++ crons/delete_invalid_links.py
@@ -1,6 +1,9 @@
 # put logic here
+from link_shortener import LinkShortener
+from json_manager import JsonManager
 
-
+# url = LinkShortener.get_link_by_id(id)
+url_entities = LinkShortener.get_all_links()
 
 
 
