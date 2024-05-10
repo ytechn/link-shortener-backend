5f6ad552d2edef6b3a493fb15a82f5ac827c9cfa
cyber <yinnonh@technion.ac.il>
2024-05-10 14:45:14 +0300

    changed location of delete service :

--- delete_invalid_links.py
+++ delete_invalid_links.py
@@ -2,8 +2,8 @@
 from link_shortener import LinkShortener
 from json_manager import JsonManager
 
-# url = LinkShortener.get_link_by_id(id)
 url_entities = LinkShortener.get_all_links()
+print("stop here")
 
 
 
