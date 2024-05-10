0f6b025c361afb1dbb78cc4d523835e0dc582385
cyber <yinnonh@technion.ac.il>
2024-05-10 13:20:50 +0300

    removed containers script and added docker-compose instead

--- run_multiple_containers.sh
+++
@@ -1,5 +0,0 @@
-docker run -p 7000:8080 -v ./data/:/app/data/ link-shortener-devops &
-docker run -p 7001:8080 -v ./data/:/app/data/ link-shortener-devops &
-docker run -p 7002:8080 -v ./data/:/app/data/ link-shortener-devops &
-
-wait
