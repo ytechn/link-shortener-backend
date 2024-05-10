1f47f41de018abb7640efbe5cfd7b91c1d0941ef
cyber <yinnonh@technion.ac.il>
2024-05-10 11:55:34 +0300

    changed path of the json links

--- json_manager.py
+++ json_manager.py
@@ -1,14 +1,16 @@
 import json
 
+FILE_NAME = "./data/links.json"
+
 class JsonManager:
     @classmethod
-    def read(self, file_name: str = "links.json"):
+    def read(self, file_name: str = FILE_NAME):
         with open(file_name) as f:
             data = json.load(f)
         return data
 
     @classmethod
-    def write(self, data, file_name: str = "links.json"):
+    def write(self, data, file_name: str = FILE_NAME):
         try:
             with open(file_name, "w") as f:
                 json.dump(data, f)
