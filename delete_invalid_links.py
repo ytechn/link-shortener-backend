6993fccd8b692e1f1fee594a2250678848efab4f
ytechn <yinnonh@technion.ac.il>
2024-05-17 12:17:16 +0300

    added invalid links functionality

--- delete_invalid_links.py
+++ delete_invalid_links.py
@@ -1,10 +1,26 @@
 # put logic here
+import requests
 from link_shortener import LinkShortener
 from json_manager import JsonManager
 
 url_entities = LinkShortener.get_all_links()
+new_url_entities = []
+
 print("stop here")
 
+def is_valid_link(link: str) -> bool:
+    try:
+        response = requests.get(link, )
+        status_code = response.status_code
+        if status_code in [200, 201]:
+            return True
+        return False
+    except:
+        return False
 
+for entity in url_entities:
+    entity_url = entity["url"]
+    if is_valid_link(entity_url):
+        new_url_entities.append(entity)
 
-
+JsonManager.write(new_url_entities)
