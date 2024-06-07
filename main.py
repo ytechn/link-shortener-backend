70e8a4977a4dd8f323c49ce931e0a286e4ac4507
ytechn <yinnonh@technion.ac.il>
2024-06-07 14:53:18 +0300

    added configurtations for logstash

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import logging
+import os
+
 from link_shortener import LinkShortener
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 
+from logging.handlers import SocketHandler
+from logstash_formatter import LogstashFormatterV1
+
+LOGSTASH_HOST = os.getenv("LOGSTASH_HOST")
+LOGSTASH_PORT = int(os.getenv("LOGSTASH_PORT"))
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logstash_handler = SocketHandler(LOGSTASH_HOST, LOGSTASH_PORT)
+logstash_handler.setFormatter(LogstashFormatterV1)
+logger.addHandler(logstash_handler)
 
 app = FastAPI()
 
@@ -12,6 +27,7 @@ def health_check(r: Request):
 @app.get("/all")
 def get_all_links():
     results = LinkShortener.get_all()
+    logging.error("FETCHED ALL LINKS")
     return results
 
 @app.post("/shorten")
